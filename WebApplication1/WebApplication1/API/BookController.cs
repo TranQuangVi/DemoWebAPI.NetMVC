c8c52a2a0f65a0745c1882290e18de0ef8c3ced1
TranQuangVi <112139716+TranQuangVi@users.noreply.github.com>
2023-03-05 08:37:21 +0700

    Create project and word

---
+++ WebApplication1/WebApplication1/API/BookController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+using WebApplication1.Models;
+
+namespace WebApplication1.API
+{
+    public class BookController : ApiController
+    {
+
+        private List<Book> ListBooks()
+        {
+            List<Book> list = new List<Book>();
+            for (int i = 1; i < 6; i++)   // Tạo ra 6 User
+            {
+                Book book = new Book();
+                book.Id = i;
+                book.Title = "Sách " + i;
+                book.Author = "Tác giả " + i;
+                book.Year = 2022;
+                book.Price = 100000;
+                book.Cover = "Images/book" + i + ".jpg";
+                list.Add(book);
+            }
+            return list;
+        }
+
+        // lấy tất cả sách
+        [HttpGet] 
+        public List<Book> GetListBooks()
+        {
+            return ListBooks();
+        }
+
+       [HttpGet]
+        public Book GetBook(int id)
+        {
+            return ListBooks().Where(b => b.Id==id).FirstOrDefault();
+        }
+
+ 
+        [HttpPost]// POST api/values   thêm mới
+        public HttpResponseMessage CreateNew(Book book)
+        {
+            try
+            {
+                var list = ListBooks();
+                list.Add(book);   
+                return Request.CreateResponse(HttpStatusCode.OK, list);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // Put: Edit thông tin sách
+        [HttpPut]
+        public HttpResponseMessage UpdateBook(Book book)
+        {
+            try
+            {
+                var list = ListBooks();
+                int index = list.FindIndex(p => p.Id == book.Id);
+                list[index] = book;   
+                return Request.CreateResponse(HttpStatusCode.OK, list);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // DELETE: Xóa sách
+        [HttpDelete]
+        public HttpResponseMessage DeleteBook(Book book)
+        {
+            try
+            {
+                var list = ListBooks();
+                int index = list.FindIndex(p => p.Id == book.Id);
+                list.RemoveAt(index);
+                return Request.CreateResponse(HttpStatusCode.OK, list);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var list = await GetListBooks();
+            return View(list);
+        }
+        private async Task<List<Book>> GetListBooks()    // Hàm Gọi API trả về list
+        {
+            string baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority +
+                Request.ApplicationPath.TrimEnd('/') + "/";   // Lấy base uri của website
+            using (var httpClient = new HttpClient())
+            {
+                HttpResponseMessage res = await httpClient.GetAsync(baseUrl + "api/Book/GetListBooks");
+                if (res.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<Book> list = new List<Book>();
+                    list = res.Content.ReadAsAsync<List<Book>>().Result;
+                    return list;
+                }
+                return null;
+            }
+        }
+
+        public async Task<ActionResult> Detail(int id)
+        {
+            var book = await GetBook(id);
+            return View(book);
+        }
+        private async Task<Book> GetBook(int id)   // Hàm Gọi API trả về Book
+        {
+            string baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority +
+                Request.ApplicationPath.TrimEnd('/') + "/";   // Lấy base uri của website
+            using (var httpClient = new HttpClient())
+            {
+                HttpResponseMessage res = await httpClient.GetAsync(baseUrl + "api/Book/GetBook/" +id);
+                if (res.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return res.Content.ReadAsAsync<Book>().Result;
+                }
+                return null;
+            }
+        }
+    }
+}
+
+
